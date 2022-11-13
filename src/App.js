0635f7e1dd4b8a4fece7a6173237f4f3c6731bf7
igor-1987 <igor.gayvoronsky@gmail.com>
2022-11-13 18:27:59 +0200

    how creating of elements looks like without jsx: using of React.createElement

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from "react";
 import ExpenseList from "./components/ExpenseList";
 import expensesFromApi from "./api/expenses";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
-      <ExpenseList
-        expenses={expensesFromApi} 
-      />
-    </div>
+  return React.createElement(
+    'div', 
+    {}, 
+    React.createElement('h2', {}, 'Let\'s get started!'),
+    React.createElement(ExpenseList, { expenses: expensesFromApi }),
   );
+  // React.createElement takes 3 arguments: 
+  // 1) element which will be created,
+  // 2) object which configures this element
+  // 3) child elements of created element
 }
 
 export default App;
